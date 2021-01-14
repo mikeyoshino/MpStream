e7b7a6f43c0eaadef8c17c5e5ddd72f3cfeee5a5
Mike Yoshino <yoshinotsubagi@gmail.com>
2021-01-14 18:17:22 +0700

    mouse over

---
+++ MpStream/Pages/Index.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using MpStream.Models;
+using MpStream.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MpStream.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public List<MovieEntity> MovieList { get; set; }
+        [Inject]
+        public MovieService MovieService { get; set; }
+        public bool IsMouseOver { get; set; } = false;
+        public Dictionary<int, bool> MouseEventMapbyMovieId { get; set; } = new Dictionary<int, bool>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            MovieList = MovieService.GetMovieList();
+            foreach (var eachMovie in MovieList)
+            {
+                MouseEventMapbyMovieId.Add(eachMovie.Id, false);
+            }
+        }
+
+        async Task MouseOver(int Id)
+        {
+            MouseEventMapbyMovieId[Id] = true;
+        }
+        async Task MouseOut(int Id)
+        {
+            MouseEventMapbyMovieId[Id] = false;
+        }
+
+    }
+}
